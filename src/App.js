5a319735c256cc94632e33c0e01ebe8a2068b011
Catevika <dominique.bello@outlook.fr>
2020-06-12 16:24:24 +0200

    Headline component with Tests

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from './component/header/index';
+import Headline from './component/headline/index';
 import './App.scss';
 
 function App() {
 	return (
 		<div className='App'>
 			<Header />
+			<section className='main'>
+				<Headline header='Posts' desc='Click the button to render Posts' />
+			</section>
 		</div>
 	);
 }
