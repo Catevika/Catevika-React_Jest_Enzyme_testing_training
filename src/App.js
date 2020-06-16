55699129dc87ce67ad2ea4ac082661af9b4cae0f
Catevika <dominique.bello@outlook.fr>
2020-06-16 17:26:07 +0200

    Tests of Methods

--- src/App.js
+++ src/App.js
@@ -19,24 +19,47 @@ const tempArr = [
 		onlineStatus: true
 	}
 ];
+
+const initialState = {
+	hideBtn: false
+};
+
 class App extends Component {
 	constructor(props) {
 		super(props);
+
+		this.state = {
+			...initialState
+		};
 
 		this.fetch = this.fetch.bind(this);
 	}
 
 	fetch() {
 		this.props.fetchPosts();
+		this.exampleMethod_updatesState();
+	}
+
+	exampleMethod_updatesState() {
+		const { hideBtn } = this.state;
+		this.setState({
+			hideBtn: !hideBtn
+		});
+	}
+
+	exampleMethod_returnsAValue(number) {
+		return number + 1;
 	}
 
 	render() {
 		const { posts } = this.props;
+		const { hideBtn } = this.state;
 
 		const configButton = {
-			buttonText: 'Gets posts',
+			buttonText: 'Get posts',
 			emitEvent: this.fetch
 		};
+
 		return (
 			<div className='App' data-test='appComponent'>
 				<Header />
@@ -46,7 +69,8 @@ class App extends Component {
 						desc='Click the button to render posts!'
 						tempArr={tempArr}
 					/>
-					<SharedButton {...configButton} />
+
+					{!hideBtn && <SharedButton {...configButton} />}
 					{posts.length > 0 && (
 						<div>
 							{posts.map((post, index) => {
@@ -72,6 +96,3 @@ const mapStateToProps = (state) => {
 };
 
 export default connect(mapStateToProps, { fetchPosts })(App);
-
-
-
